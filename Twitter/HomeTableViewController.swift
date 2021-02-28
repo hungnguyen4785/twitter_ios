5553f421aabe1a64eeccfaac145386c3f8351aee
hungnguyen4785 <hung.t.nguyen03@sjsu.edu>
2021-02-28 02:08:05 -0800

    add favorite and retweet function

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -17,9 +17,16 @@ class HomeTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweets()
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        self.loadTweets()
+        loadTweets()
     }
         
     @objc func loadTweets() {
@@ -89,6 +96,11 @@ class HomeTableViewController: UITableViewController {
             cell.profileImageVIew.image = UIImage(data: imageData)
     
         }
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
+        
         
         return cell
     }
